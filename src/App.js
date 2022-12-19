b6d19872fd84c6f0df6861b240de5e4aa142969d
sikkdev <brandonk3nt@gmail.com>
2022-12-19 08:02:08 -0700

    base setup for json view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+async function getTestData() {
+  const raw = await fetch('https://raw.githubusercontent.com/epic404/json-tree/master/data/test1.json');
+  const data = await raw.json();
+  return data;
+}
+
+function Node() {
+  const [isExpanded, setExpanded] = useState(false);
+  return (
+    <div>
+      <span
+        className={`material-symbols-outlined ${isExpanded ? 'expanded' : 'collapsed'}`}
+        onClick={() => setExpanded(!isExpanded)}>
+          arrow_right
+      </span>
+    </div>
+  )
+}
 
 function App() {
+  useEffect(() => {
+    const data = getTestData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Node />
     </div>
   );
 }
